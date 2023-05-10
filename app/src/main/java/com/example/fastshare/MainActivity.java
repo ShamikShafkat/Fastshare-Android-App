73322ff924cfa8a010dbe782443882740159c240
galibmahmudjim <galibmahmudjim@gmail.com>
2023-05-11 01:43:01 +0600

    logo, splash screen and some card

--- app/src/main/java/com/example/fastshare/MainActivity.java
+++ app/src/main/java/com/example/fastshare/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this,receiveActivity.class);
             startActivity(intent);
         });
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
